20e311cc1061ac1896596435b9944b93fde3e35d
Sebi Nemeth <>
2023-10-11 12:08:22 +0200

    init

---
+++ custom_components/flexopus/image.py
@@ -0,0 +1,15 @@
+from .data_coordinator import DataCoordinator
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+
+from .const import DOMAIN
+
+
+async def async_setup_entry(
+    hass: HomeAssistant,
+    entry: ConfigEntry,
+    async_add_entities: AddEntitiesCallback,
+) -> None:
+    coordinator: DataCoordinator = hass.data[DOMAIN][entry.entry_id]
+    map = coordinator.api.fetch_map()
